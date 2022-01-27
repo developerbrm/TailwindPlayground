c152689c8fcc68a9d0bf06cc019f2e32c22c665f
Braham Prakash <braham.prakash@pristyncare.com>
2022-01-28 01:45:57 +0530

    - initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="h-screen">
+        <section className="text-test h-screen bg-gray-900 grid place-content-center text-center">
+          <h2 className="text-sky-500 text-5xl mb-3 font-medium">
+            Welcome to Tailwind CSS
+          </h2>
+          <p className="text-sky-500 max-w-prose">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur
+            nihil commodi tempore earum suscipit vero? Tempore sint nobis,
+            cumque labore tempora facilis eum odit pariatur, debitis autem
+            rerum! Doloribus iusto quibusdam laboriosam recusandae eaque id
+            quisquam quidem quaerat laborum reiciendis!
+          </p>
+
+          <p className="text-sky-500 text-lg my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+        </section>
+
+        <section className="h-screen bg-stone-100 grid place-content-center text-center">
+          <h2 className="text-emerald-700 text-5xl mb-3 font-medium">
+            Writing CSS was never easier
+          </h2>
+
+          <p className="text-emerald-700 max-w-prose">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nisi
+            ullam maiores illum culpa minima deleniti veritatis eius, recusandae
+            ad? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nesciunt illum incidunt obcaecati similique voluptatem eaque culpa
+            sed? Architecto dignissimos libero veritatis nobis at optio nihil
+            fugiat consequuntur iusto eos pariatur nulla illo doloremque aliquid
+            sequi hic, reiciendis iure? Vel, et doloremque? Provident totam
+            tempore, laborum incidunt culpa unde illo? Iure.
+          </p>
+        </section>
+
+        <section className="h-screen bg-rose-600 grid place-content-center text-center">
+          <h2 className="text-white text-5xl mb-3 font-medium">
+            See these beautiful Colors
+          </h2>
+
+          <p className="text-white max-w-prose">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit expedita nemo ipsam nobis nostrum consequuntur rerum
+            officiis inventore. Possimus impedit perspiciatis numquam voluptatem
+            dolorum harum tempore, suscipit quas quibusdam, vitae explicabo,
+            beatae facilis dolore ipsum quae laudantium culpa nam id aliquid
+            nihil. Sit, minus magni?
+          </p>
+        </section>
+
+        <section className="h-screen bg-emerald-300 grid place-content-center text-center">
+          <h2 className="text-slate-800 text-5xl mb-3 font-medium">
+           Good
+          </h2>
+
+          <p className="text-slate-800 max-w-prose">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit expedita nemo ipsam nobis nostrum consequuntur rerum
+            officiis inventore. Possimus impedit perspiciatis numquam voluptatem
+            dolorum harum tempore, suscipit quas quibusdam, vitae explicabo,
+            beatae facilis dolore ipsum quae laudantium culpa nam id aliquid
+            nihil. Sit, minus magni?
+          </p>
+        </section>
+
+        <section className="h-screen bg-purple-200 grid place-content-center text-center">
+          <h2 className="text-purple-900 text-5xl mb-3 font-medium">
+          Sweet
+          </h2>
+
+          <p className="text-purple-900 max-w-prose">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit expedita nemo ipsam nobis nostrum consequuntur rerum
+            officiis inventore. Possimus impedit perspiciatis numquam voluptatem
+            dolorum harum tempore, suscipit quas quibusdam, vitae explicabo,
+            beatae facilis dolore ipsum quae laudantium culpa nam id aliquid
+            nihil. Sit, minus magni?
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
